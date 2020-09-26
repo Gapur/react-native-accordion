4bc3effb3bb591ece17cabe26a4ee47aba451853
Gapur Kassym <gapur.kassym@gmail.com>
2020-09-26 22:18:39 +0600

    feat(app): eslint prettier

--- index.ts
+++ index.ts
@@ -1,12 +1,9 @@
-const add = (x: number, y:number):number => {
+const add = (x: number, y: number): number => {
   return x + y;
-}
+};
 
-const subtract = (x: number, y:number):number => {
+const subtract = (x: number, y: number): number => {
   return x - y;
-}
+};
 
-export {
-  add,
-  subtract
-}
+export { add, subtract };
