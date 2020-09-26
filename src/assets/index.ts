65e6af8652cf6b3b0f4b08bee3913bfb467444ad
Gapur Kassym <gapur.kassym@gmail.com>
2020-09-26 23:50:57 +0600

    feat(src): create accordion

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export const images = {
+  arrowUp: require("./arrow-up.png"),
+  arrowDown: require("./arrow-down.png"),
+};
---
+++ src/index.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, LayoutAnimation, TouchableOpacity, Platform, UIManager } from "react-native";
+
+import { images } from "./assets";
+
+interface AccordionProps {
+  headerTitle: string;
+  children: React.ReactNode;
+  renderHeader?: () => void;
+}
+
+if (Platform.OS === "android" && UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
+export default function Accordion({ headerTitle, children, renderHeader }: AccordionProps) {
+  const [showContent, setShowContent] = useState(false);
+  const renderAccordionHeader = () => {
+    if (renderHeader) {
+      return renderHeader();
+    }
+    return <Text>{headerTitle}</Text>;
+  };
+
+  const handleToggleContent = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    setShowContent(!showContent);
+  };
+
+  return (
+    <View>
+      {renderAccordionHeader()}
+      {showContent && children}
+      <TouchableOpacity style={styles.arrowBtn} onPress={handleToggleContent}>
+        <Image style={styles.arrowBtnIcon} source={showContent ? images.arrowUp : images.arrowDown} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  arrowBtn: {
+    alignItems: "center",
+  },
+  arrowBtnIcon: {
+    height: 10,
+    width: 16.5,
+  },
+});
